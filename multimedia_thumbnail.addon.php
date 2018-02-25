ae7ecacc28a0f8f808be90cc4979310fc68b8507
Jin Hu, Baek <m@ake.kr>
2018-02-25 14:43:04 +0900

    2.5.0-beta
    none

--- multimedia_thumbnail.addon.php
+++ multimedia_thumbnail.addon.php
@@ -3,7 +3,7 @@
    * @file Multimedia_thumbnail.addon.php
    * @author Jin Hu, Baek(m@ake.kr, @mAKEkr)
    * @brief generate thumbnail to  multimedia in document.
-   * @version 2.2.0
+   * @version 3.0.0
    ***/
 
   if ($called_position === 'before_module_proc')
@@ -15,61 +15,27 @@
 
       $oContext = Context::getInstance();
       $vars = Context::getRequestVars();
-      $content = $vars->content;
-      $MultimediaThumb = new MultimediaThumb($addon_info);
+      $MultimediaThumb = new MultimediaThumb($addon_info, $vars->content, $vars->document_srl);
 
-      if ($MultimediaThumb->checkDocumentThumbExsits($vars->document_srl, $content))
-      {
-        $multimedia_list = $MultimediaThumb->getMultimediaList($content);
-
-        if ($MultimediaThumb->checkMultimediaThumbExists($content))
-        {
-          if (count($multimedia_list) !== 0)
-          {
-            $multimedia_thumb = $MultimediaThumb->getMultimediaThumb($content);
-            $new_multimedia_thumb = $MultimediaThumb->getMultimediaFormat($multimedia_list['0']);
-
-            if ($multimedia_thumb['0'] != $new_multimedia_thumb)
-            {
-              $result_content = $MultimediaThumb->removeMultimediaThumb($content);
-              $result_content = $MultimediaThumb->makeMultimediaThumb($new_multimedia_thumb) . $result_content;
-            }
-          } else {
-            $result_content = $MultimediaThumb->removeMultimediaThumb($content);
-          }
-        }
-      } else {
-        $multimedia_list = $MultimediaThumb->getMultimediaList($content);
-
-        if (count($multimedia_list) !== 0)
-        {
-          $new_multimedia_thumb = $MmultimediaThumb->getMultimediaFormat($multimedia_list['0']);
-
-          if ($multimedia_thumb['0'] != $new_multimedia_thumb)
-          {
-            $result_content = $MultimediaThumb->makeMultimediaThumb($new_multimedia_thumb) . $content;
-          }
-        }
-      }
-
-      $oContext->set('content', ($result_content === NULL) ? $vars->content : $result_content, TRUE);
+      $oContext->set('content', $MultimediaThumb->getResult(), TRUE);
 
       unset($oContext);
       unset($vars);
       unset($content);
     }
   } else if ($called_position === 'after_module_proc') {
-    if ($this->act === 'dispBoardContent' || $this->act === 'dispBoardWrite')
+    if (($this->act === 'dispBoardContent' || $this->act === 'dispBoardWrite') &&
+        (int)Context::get('document_srl') !== 0)
     {
-      include_once(_XE_PATH_ . 'addons/ultimedia_thumbnail/multimedia_thumbnail.class.php');
+      include_once(_XE_PATH_ . 'addons/multimedia_thumbnail/multimedia_thumbnail.class.php');
 
       $oDocument = Context::get('oDocument');
-
       $MultimediaThumb = new MultimediaThumb($addon_info);
-      $oDocument->variables['content'] = ($this->act === 'dispBoardContent') ?
-        $MultimediaThumb->hideMultimediaThumb($oDocument->variables['content']) :
-        $MultimediaThumb->removeMultimediaThumb($oDocument->variables['content']);
 
-      unset($oDocument);
+      $oDocument->variables['content'] = ($this->act === 'dispBoardContent') ?
+        $MultimediaThumb->filterMultimediaThumb('hide', $oDocument->variables['content']) :
+        $MultimediaThumb->filterMultimediaThumb('remove', $oDocument->variables['content']);
+
+      // unset($oDocument);
     }
   }
--- multimedia_thumbnail.class.php
+++ multimedia_thumbnail.class.php
@@ -1,97 +1,118 @@
 <?php
   class MultimediaThumb{
     var $options;
+    var $document;
 
-    function __construct($options){
+    function __construct ($options, $content = NULL, $document_srl = 0) {
       if($options->mt_thumbnailres_youtube     === NULL) $options->mt_thumbnailres_youtube     = 'hqdefault';
       if($options->mt_thumbnailres_soundcloud  === NULL) $options->mt_thumbnailres_soundcloud  = 't300x300';
       if($options->mt_thumbnailres_vimeo       === NULL) $options->mt_thumbnailres_vimeo       = 'thumbnail_medium';
       if($options->mt_thumbnailres_dailymotion === NULL) $options->mt_thumbnailres_dailymotion = 'thumbnail_240_url';
 
       $this->options = $options;
-      // Temporary builded regular expression. maybe fixed some reason.
-      $this->validateRegExr = '/(?:<img|(?<!^)\G)\h*(\w+)="([^"]+)"(?=.*?\/>)/g';
+
+      // Compatibillity in ver 3.x
+      // 하위호환성 확보를 위하여 Object가 아닌 Array로 제작.
+      $this->variables = Array(
+        'document_srl' => $document_srl
+      );
+      $this->document = Array(
+        'content' => $content,
+        'multimediaList' => Array(),
+        'imageList' => Array(),
+        'isMakeThumbnail' => false, // 애드온을 통하여 섬네일을 생성한 경우
+        'isHaveThumbnail' => false // 첨부파일로 인하여 섬네일을 갖고있을 경우
+      );
+
+      if ($this->document['content'] !== NULL) {
+        $this->proccessDocument();
+      }
     }
 
-    function checkDocumentThumbExsits($document_srl, $content) {
+    /*
+      1. 게시물에 파일이 존재하는지 확인
+      2. 게시물에 이미지가 존재하는지 확인
+      3. 게시물에 멀티미디어가 존재하는지 확인
+      4. 섬네일 생성
+    */
+    function proccessDocument () {
+      if ($this->document['content'] !== NULL) {
+        $this->document['imageList'] = $this->checkDocumentImages($this->document['content']);
+        $this->checkDocumentThumbExsits();
+        $this->checkDocumentEmbed();
+
+        if ($this->document['isMakeThumbnail'] === false &&
+            $this->document['isHaveThumbnail'] === false &&
+            count($this->document['multimediaList']) > 0) {
+          $this->document['content'] = $this->makeMultimediaThumb($this->document['multimediaList']['0']) . $this->document['content'];
+        }
+      }
+    }
+
+    function checkDocumentImages ($content) {
+      $dom = new DOMDocument;
+      $dom->loadHTML($content, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+
+      $imageList = Array();
+      foreach ($dom->getElementsByTagName('img') as $element) {
+        array_push($imageList, Array(
+          'node' => substr($dom->saveHTML($element), 0, -1) . ' />',
+          'class' => $element->getAttribute('class'),
+          'src' => $element->getAttribute('src'),
+          'alt' => $element->getAttribute('alt'), // compatibility in 2.x
+          'rel' => $element->getAttribute('rel') // compatibility in 1.x and 2.0, 2.1
+        ));
+      }
+
+
+      return $imageList;
+    }
+
+    function checkDocumentThumbExsits () {
+      $document_srl = $this->variables['document_srl'];
       $oContext = Context::getInstance();
       $oFileModel = getModel('file');
 
       $attachment_list = $oFileModel->getFiles($document_srl, array(), 'file_srl', true);
 
-      if (count($attachment_list) != '0') { // 첨부파일이 존재할경우
+      if (count($attachment_list) > 0) { // 첨부파일이 존재할경우
         // 체크할 첨부파일의 확장자가 아래와 같을경우
         $allow_attachment_extension = Array('jpg', 'jpeg', 'gif', 'png');
 
         foreach ($attachment_list as $val) {
-          if( ! in_array(strtolower(pathinfo($val->uploaded_filename, PATHINFO_EXTENSION)), $allow_attachment_extension)) continue;
-          else if( ! file_exists($val->uploaded_filename)) continue;
-          else
-            $return_value = true;
-          break;
+          if (in_array(strtolower(pathinfo($val->uploaded_filename, PATHINFO_EXTENSION)), $allow_attachment_extension) === false || file_exists($val->uploaded_filename) === false) {
+            continue;
+          } else {
+            $this->document['isHaveThumbnail'] = true;
+            break;
+          }
         }
       } else { // 첨부파일이 존재하지 않을 경우
         // 이미지가 존재하는지 체크 인데 멀티미디어 섬네일이 존재하는지 체크
-        preg_match_all($this->validateRegExr, $content, $matches);
-
-        // 이미지 항목별로 반복문 재생
-        foreach ($matches as $key => $val) {
-          if ($val['0'] === null) continue;
-          else
-            $return_value = true;
-        }
-        unset($matches);
-      }
-
-      return ($return_value === NULL) ? false : $return_value;
-    }
-
-    function checkMultimediaThumbExists($content) {
-      /*
-        검사방법 - 섬네일 애드온으로 생성되었는지 확인.
-        1. 구버전을 체크하기 위하여 img 태그를 전체 긁음
-        2. class=xe-MultimediaThumb / alt=(value) / rel=(value) 가 있는지 확인
-        3. class에 xe-MultimediaThumb가 존재한다면 현재 최신코드, 2.2.x 이상대에서 제작된 섬네일
-        4. rel에 multimedia format에 맞는 code가 들어가있는 경우 구버전대(1.x대, 2.0.x에서 제작된 섬네일)
-        5. alt에 multimedia format에 맞는 code가 들어가있는 경우 신버전대(2.1.x대에서 제작된 섬네일)
-      */
-      preg_match_all($this->validateRegExr, $content, $matches);
-
-      $return_value = '';
-
-      foreach($matches['2'] as $key => $val){
-        $return_value .= $val;
-
-        if ($val == null) continue;
-        else {
-          $return_value = $this->checkMultimediaFormat($val);
-          if($return_value === true) break;
+        foreach ($this->document['imageList'] as $element) {
+          if ($element['class'] === 'xe-MultimediaThumb' ||
+              strpos($element['alt'], ':') > 0 ||
+              strpos($element['rel'], ':') > 0) {
+            $this->document['isMakeThumbnail'] = true;
+            break;
+          }
         }
       }
 
-      unset($matches);
-      return ($return_value === NULL) ? false : $return_value;
+      return ($this->document['isMakeThumbnail'] !== false && $this->document['isHaveThumbnail'] !== false) ? true : false;
     }
 
-    function getMultimediaThumb($content) {
-      preg_match_all($this->validateRegExr, $content, $matches);
-      $return_value = array();
+    function checkDocumentEmbed () {
+      preg_match_all('/<(?=embed|iframe)[^>]+src=["\']?([^>"\']+)["\'].*?[^>]>/i', $this->document['content'], $matches);
 
-      foreach($matches['2'] as $key => $val){
-        if($val === NULL) continue;
-        else {
-          array_push($return_value, $val); // format
-          array_push($return_value, $matches['1'][$key]); // url
-          break;
-        }
+      foreach ($matches['1'] as $embed) {
+        array_push($this->document['multimediaList'], $this->getMultimediaFormat($embed));
       }
 
-      if($return_value['0'] == NULL) $return_value = false;
-
-      return $return_value;
+      return $this->document['multimediaList'];
     }
 
-    function checkMultimediaFormat($format) {
+    function checkMultimediaFormat ($format) {
       list($service_name, $service_id) = explode(':', $format);
 
       /*
@@ -140,13 +161,7 @@
       return $return_value;
     }
 
-    function getMultimediaList($content) {
-      preg_match_all('/<(?=embed|iframe)[^>]+src=["\']?([^>"\']+)["\'].*?[^>]>/i', $content, $matches);
-
-      return $matches['1'];
-    }
-
-    function getMultimediaFormat($url) {
+    function getMultimediaFormat ($url) {
       //youtube
       preg_match('/youtube(?:|-nocookie).com\/(?:(?:v|embed)\/)?([a-zA-Z0-9-_]+)/i', $url, $youtube);
       //vimeo
@@ -177,10 +192,11 @@
       unset($pandora);
       unset($dailymotion);
 
+
       return ($return_value === NULL) ? false : $return_value;
     }
 
-    function makeMultimediaThumb($format) {
+    function makeMultimediaThumb ($format) {
       list($service_name, $service_id) = explode(':', $format);
       switch($service_name){
         case 'youtube':
@@ -195,37 +211,37 @@
         break;
 
         case 'vimeo':
-          $json = json_decode(FileHandler::getRemoteResource('http://vimeo.com/api/v2/video/' . $service_id . '.json', null, null, 'GET', 'application/json'));
+          $json = json_decode(FileHandler::getRemoteResource('http://vimeo.com/api/v2/video/' . $service_id . '.json', NULL, NULL, 'GET', 'application/json'));
           $thumbnail_url = $json['0']->{$this->options->mt_thumbnailres_vimeo};
         break;
 
         case 'soundcloud':
-          $json = json_decode(FileHandler::getRemoteResource('http://api.soundcloud.com/tracks/' . $service_id . '.json?client_id=2f426cafc5cec317628b1db5224fc7a1', null, null, 'GET', 'application/json'));
+          $json = json_decode(FileHandler::getRemoteResource('http://api.soundcloud.com/tracks/' . $service_id . '.json?client_id=2f426cafc5cec317628b1db5224fc7a1', NULL, NULL, 'GET', 'application/json'));
           $thumbnail_url = str_replace('large', $this->options->mt_thumbnailres_soundcloud, $json->artwork_url);
           $thumbnail_url = substr($thumbnail_url, 0, strpos($thumbnail_url, '?'));
         break;
 
         case 'daum':
-          $xml = simplexml_load_string(FileHandler::getRemoteResource('http://tvpot.daum.net/clip/ClipInfoXml.do?vid=' . $service_id, null, null, 'GET', 'text/xml'), 'SimpleXMLElement', LIBXML_NOCDATA | LIBXML_NOBLANKS);
+          $xml = simplexml_load_string(FileHandler::getRemoteResource('http://tvpot.daum.net/clip/ClipInfoXml.do?vid=' . $service_id, NULL, NULL, 'GET', 'text/xml'), 'SimpleXMLElement', LIBXML_NOCDATA | LIBXML_NOBLANKS);
           $thumbnail_url = $xml->THUMB_URL;
           unset($xml);
         break;
 
         case 'naver':
           list($service_id, $service_key) = explode('|', $service_id);
-          $xml = simplexml_load_string(FileHandler::getRemoteResource('http://serviceapi.nmv.naver.com/flash/videoInfo.nhn?vid=' . $service_id . '&outKey=' . $service_key, null, null, 'GET', 'text/xml'), 'SimpleXMLElement', LIBXML_NOCDATA | LIBXML_NOBLANKS);
+          $xml = simplexml_load_string(FileHandler::getRemoteResource('http://serviceapi.nmv.naver.com/flash/videoInfo.nhn?vid=' . $service_id . '&outKey=' . $service_key, NULL, NULL, 'GET', 'text/xml'), 'SimpleXMLElement', LIBXML_NOCDATA | LIBXML_NOBLANKS);
           $thumbnail_url = $xml->CoverImage;
           unset($xml);
         break;
 
         case 'pandora':
           list($service_userid, $service_id) = explode('|', $service_id);
-          $json = json_decode(FileHandler::getRemoteResource('http://flvr.pandora.tv/flv2pan/embed.dll/info?url=&prgid=' . $service_id . '&userid=' . $service_userid, null, null, 'GET', 'text/html'));
+          $json = json_decode(FileHandler::getRemoteResource('http://flvr.pandora.tv/flv2pan/embed.dll/info?url=&prgid=' . $service_id . '&userid=' . $service_userid, NULL, NULL, 'GET', 'text/html'));
           $thumbnail_url = $json->image;
         break;
 
         case 'dailymotion':
-          $json = json_decode(FileHandler::getRemoteResource('https://api.dailymotion.com/video/' . $service_id . '?fields=' . $this->options->mt_thumbnailres_dailymotion, null, null, 'GET', 'application/json'));
+          $json = json_decode(FileHandler::getRemoteResource('https://api.dailymotion.com/video/' . $service_id . '?fields=' . $this->options->mt_thumbnailres_dailymotion, NULL, NULL, 'GET', 'application/json'));
           $thumbnail_url = $json->{$this->options->mt_thumbnailres_dailymotion};
         break;
 
@@ -240,53 +256,54 @@
       unset($service_id);
       unset($service_userid);
 
-      if(is_array($thumbnail_url)) {
-        $return_string = '';
-
+      $return_string = '';
+      if (is_array($thumbnail_url)) {
         foreach($thumbnail_url as $val){
           $return_string .= '<img class="xe-MultimediaThumb" src="' . $val. '" alt="' . $format . '" />';
         }
-
-        return ($this->checkXEVersion()) ? '<!-- ' . $return_string . ' -->' : $return_string . "\n";
-        unset($return_string);
       } else {
-        $thumbnail_url = ($thumbnail_url !== false) ? '<img src="' . $thumbnail_url . '" alt="' . $format . '" />' : false;
-        if ($thumbnail_url !== false) {
-          $thumbnail_url = $thumbnail_url . "\n";
-        }
-
-        return $thumbnail_url;
+        $return_string = ($thumbnail_url !== false) ? '<img class="xe-MultimediaThumb" src="' . $thumbnail_url . '" alt="' . $format . '" />' . "\n" : false;
       }
 
+      return $return_string;
+      unset($return_string);
       unset($thumbnail_url);
       unset($format);
     }
 
-    function filterMultimediaThumb($type = 'hide', $content) {
-      if ($type === 'hide')
-      {
-        // $content = preg_replace($this->validateRegExr, '<!-- $0 -->', $content);
-      } else { // $type === 'remove'
+    public function getResult() {
+      return $this->document['content'];
+    }
+
+    function filterMultimediaThumb ($type = 'hide', $content = NULL) {
+      if ($content !== NULL) {
+        $images = $this->checkDocumentImages($content);
+        $MultimediaThumbList = Array();
+
+        foreach ($images as $element) {
+          if ($element['class'] === 'xe-MultimediaThumb' ||
+              strpos($element['alt'], ':') > 0 ||
+              strpos($element['rel'], ':') > 0) {
+            array_push($MultimediaThumbList, $element);
+          }
+        }
+
+        if ($type === 'hide')
+        {
+          foreach ($MultimediaThumbList as $element) {
+            $content = str_replace($element['node'], '<!--' . $element['node'] . '-->', $content);
+          }
+        } else { // $type === 'remove'
+          foreach ($MultimediaThumbList as $element) {
+            $content = str_replace($element['node'], '', $content);
+          }
+        }
+
+        unset($images);
+        unset($MultimediaThumbList);
       }
 
       return $content;
       unset($content);
-    }
-
-    /* Additional Scripts(common function or methods)
-    */
-
-    function getAddonConfig() {
-      return $this->options;
-    }
-
-    function checkXEVersion() {
-      list($major, $minor, $patch) = explode('.', __XE_VERSION__);
-      if(($major >= 1) && ($minor >= 8))
-      {
-        return true;
-      } else {
-        return false;
-      }
     }
   }
